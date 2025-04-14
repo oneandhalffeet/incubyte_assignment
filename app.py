721bf70ee613bc242e3f8f209b204c026752127f
oneandhalfeet <uddeshteke007@gmail.com>
2025-04-14 15:47:18 +0530

    added tests for custom delimiters

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ def add(numbers):
     if numbers == "":
         return 0
     else:
+        delimiter = ""
+        if numbers[0:2] == "//":
+            numbers = numbers[2:]
+            delimiter = numbers[0]
+            numbers = numbers.replace(numbers[0], " ")
         for i in numbers:
             if i == ",":
                 numbers = numbers.replace(i, " ")
             elif i == "\n":
+                numbers = numbers.replace(i, " ")
+            elif i == delimiter:
                 numbers = numbers.replace(i, " ")
         numbers = numbers.split()
         sum = 0
--- test_app.py
+++ test_app.py
@@ -20,6 +20,13 @@ def test_add_for_newline_separated_values():
     assert add("1\n2,3\n4") == 10
     print("✓ All add tests passed-test_add_for_newline_separated_values")
 
+def test_custom_delimiter():
+    assert add("//;\n1;2") == 3
+    assert add("//^\n1^2^3") == 6
+    assert add("//*\n1*2*3*4") == 10
+    assert add("//;\n1;2,3\n4") == 10
+    print("✓ All add tests passed-test_custom_delimiter")
+
 
 
 if __name__ == "__main__":
@@ -27,6 +34,7 @@ if __name__ == "__main__":
         test_add_for_empty_string()
         test_add_for_comma_separated_values()
         test_add_for_newline_separated_values()
+        test_custom_delimiter()
     except ImportError:
         print("❌ Test failed: calculator.py or add function not found")
     except AssertionError as e:
